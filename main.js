e633f9c3d311a7e9f31f1cf1e0402cd8d9148f3d
Daniela Welch <danielaghenascerbina@gmail.com>
2023-05-04 19:48:42 -0500

    add node server

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  //when click hamburger menu button, takes you to menu page, then click X to exit menu
+  const menuOverlay = document.querySelector(".menu-overlay");
+  const hamburgerMenu = document.querySelector("#menu");
+  hamburgerMenu.addEventListener("click", () => {
+    menuOverlay.classList.remove("invisible");
+  });
 
+  const closeButton = document.querySelector("#close");
+  closeButton.addEventListener("click", () => {
+    menuOverlay.classList.add("invisible");
+  });
 
-//when click hamburger menu button, takes you to menu page, then click X to exit menu
-const menuOverlay = document.querySelector(".menu-overlay")
-const hamburgerMenu = document.querySelector("#menu")
-hamburgerMenu.addEventListener("click", () => {
-    menuOverlay.classList.remove("invisible")
-})
+  //when click submit, takes to thankyou page, then click X to exit thank you page
+  const thankYouOverlay = document.querySelector(".thankyou");
+  const sumbitButton = document.querySelector("#submit");
+  const name = document.querySelector("#name");
+  const message = document.querySelector("#message");
+  const number = document.querySelector("#phone-number");
+  const email = document.querySelector("#email");
+  sumbitButton.addEventListener("click", () => {
+    // thankYouOverlay.classList.remove("invisible")
+    console.dir(name);
+    console.log({
+      name: name.value,
+      msg: message.value,
+      number: number.value,
+      email: email.value,
+    });
 
-const closeButton = document.querySelector("#close")
-closeButton.addEventListener("click", () => {
-    menuOverlay.classList.add("invisible")
-})
+    fetch("http://localhost:4000", {
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    })
+      .then((response) => {
+        return response
+          .text()
+          .then((text) => {
+            console.log(text);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
 
-
-//when click submit, takes to thankyou page, then click X to exit thank you page
-const thankYouOverlay = document.querySelector(".thankyou")
-const sumbitButton = document.querySelector("#submit")
-sumbitButton.addEventListener("click", () => {
-    thankYouOverlay.classList.remove("invisible")
-})
-
-const closeSecButton = document.querySelector("#close-thank")
-closeSecButton.addEventListener("click", () => {
-    thankYouOverlay.classList.add("invisible")
-})
-
-})
+  const closeSecButton = document.querySelector("#close-thank");
+  closeSecButton.addEventListener("click", () => {
+    thankYouOverlay.classList.add("invisible");
+  });
+});
