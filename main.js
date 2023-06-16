bb62baecc4155acd29bf92948f2eae8f842f2f06
Daniela Welch <danielaghenascerbina@gmail.com>
2023-06-16 14:45:11 -0500

    fixed javascript according to changes

--- main.js
+++ main.js
@@ -1,109 +1,93 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //when click hamburger menu button, takes you to menu page, then click X to exit menu
-  const menuOverlay = document.querySelector(".menu-overlay");
-  const hamburgerMenu = document.querySelector("#menu");
-  hamburgerMenu.addEventListener("click", () => {
-    menuOverlay.classList.remove("invisible");
-  });
+//when click submit, takes to thankyou page, then click X to exit thank you page
+const thankYouOverlay = document.querySelector(".thankyou");
+const submitButton = document.querySelector("#submit");
+const custName = document.querySelector("#name");
+const message = document.querySelector("#message");
+const number = document.querySelector("#phone-number");
+const email = document.querySelector("#email");
+const error = document.querySelector("#form-error");
+const form = document.querySelector("#contactForm");
 
-  const closeButton = document.querySelector("#close");
-  closeButton.addEventListener("click", () => {
-    menuOverlay.classList.add("invisible");
-  });
+/* can exit thank you page using the esc key*/
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    thankYouOverlay.classList.add("invisible");
+  }
+});
 
-  //when click submit, takes to thankyou page, then click X to exit thank you page
-  const thankYouOverlay = document.querySelector(".thankyou");
-  const submitButton = document.querySelector("#submit");
-  const name = document.querySelector("#name");
-  const message = document.querySelector("#message");
-  const number = document.querySelector("#phone-number");
-  const email = document.querySelector("#email");
-  const error = document.querySelector("#form-error");
-  const form = document.querySelector("#contactForm");
-
-  /* can exit hamburger menu and thank you pages using the
-  esc key*/
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      menuOverlay.classList.add("invisible");
-      thankYouOverlay.classList.add("invisible");
-    }
-  });
-
-  /*disables button until text is typed into name & message input 
+/*disables button until text is typed into name & message input 
   areas (the 'required' fields),and checks if there is text in 
   either the phone or email text area,then enables, 
   then disables again when text is deleted.*/
-  const isValid = () => {
-    return (
-      name.value.length > 0 &&
-      message.value.length > 0 &&
-      (email.value.length > 0 || number.value.length > 0)
-    );
-  };
-  const inputCheck = () => {
-    if (isValid()) {
-      submitButton.removeAttribute("disabled");
-    } else {
-      submitButton.setAttribute("disabled", true);
-    }
-  };
-  inputCheck();
-  name.addEventListener("input", () => {
-    inputCheck();
-  });
-
-  message.addEventListener("input", () => {
-    inputCheck();
-  });
-
-  number.addEventListener("input", () => {
-    inputCheck();
-  });
-
-  email.addEventListener("input", () => {
-    inputCheck();
-  });
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
+const isValid = () => {
+  return (
+    custName.value.length > 0 &&
+    message.value.length > 0 &&
+    (email.value.length > 0 || number.value.length > 0)
+  );
+};
+const inputCheck = () => {
+  if (isValid()) {
+    submitButton.removeAttribute("disabled");
+  } else {
     submitButton.setAttribute("disabled", true);
+  }
+};
+inputCheck();
+custName.addEventListener("input", () => {
+  inputCheck();
+});
 
-    // replaces word submit on the submit button with an svg of a loading icon
-    const loadingImg = document.createElement("img");
-    loadingImg.setAttribute("src", "./bubble-loading.svg");
-    submitButton.innerHTML = "";
-    submitButton.appendChild(loadingImg);
+message.addEventListener("input", () => {
+  inputCheck();
+});
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+number.addEventListener("input", () => {
+  inputCheck();
+});
+
+email.addEventListener("input", () => {
+  inputCheck();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  submitButton.setAttribute("disabled", true);
+
+  // replaces word submit on the submit button with an svg of a loading icon
+  const loadingImg = document.createElement("img");
+  loadingImg.setAttribute("src", "./bubble-loading.svg");
+  submitButton.innerHTML = "";
+  submitButton.appendChild(loadingImg);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      // makes the thank you page visible when submit button is clicked
+      thankYouOverlay.classList.remove("invisible");
     })
-      .then(() => {
-        // makes the thank you page visible when submit button is clicked
-        thankYouOverlay.classList.remove("invisible");
-      })
-      .catch((err) => {
-        console.error(err);
-        // if theres an error, shows an error message above button
-        error.classList.remove("invisible");
-      })
+    .catch((err) => {
+      console.error(err);
+      // if theres an error, shows an error message above button
+      error.classList.remove("invisible");
+    })
 
-      .finally(() => {
-        // re enables button after info is submitted
-        submitButton.removeAttribute("disabled");
-        submitButton.innerHTML = "Submit";
-      });
-  });
+    .finally(() => {
+      // re enables button after info is submitted
+      submitButton.removeAttribute("disabled");
+      submitButton.innerHTML = "Submit";
+    });
+});
 
-  // closes the thank you page using the X
-  const closeSecButton = document.querySelector("#close-thank");
-  closeSecButton.addEventListener("click", () => {
-    thankYouOverlay.classList.add("invisible");
-  });
+// closes the thank you page using the X
+const closeSecButton = document.querySelector("#close-thank");
+closeSecButton.addEventListener("click", () => {
+  thankYouOverlay.classList.add("invisible");
 });
